471c43202595181fe2c37590ae751606a05a7652
NickLangat <nickson@reveall.co>
2023-05-13 23:36:21 +0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.text import slugify
+from accounts.models import UserAccount
+# Create your models here.
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Project(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True, blank=True)
+    hashtags = models.ManyToManyField(Hashtag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
