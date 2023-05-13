6529b5bd8b44a78d4a1c4adb3e0746ef54b00b3d
NickLangat <nickson@reveall.co>
2023-05-13 23:58:12 +0300

    added project, hashtags, serializers and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project, Hashtag
+from .serializers import ProjectSerializer, HashtagSerializer
 
 # Create your views here.
+
+class ProjectViewset(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class HashtagViewset(viewsets.ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+
